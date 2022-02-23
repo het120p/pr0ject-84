30f93ea5c447754526c7a17b98cabbd64e9937ed
het120p <86822029+het120p@users.noreply.github.com>
2022-02-23 20:05:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWeSZtuWZN_f8EOl84D7MvlecxUaOJUeg",
+  authDomain: "spectagram-9d405.firebaseapp.com",
+  databaseURL: "https://spectagram-9d405-default-rtdb.firebaseio.com",
+  projectId: "spectagram-9d405",
+  storageBucket: "spectagram-9d405.appspot.com",
+  messagingSenderId: "436581275760",
+  appId: "1:436581275760:web:fe31c13e0f0707f1ab7cb5"
+};
